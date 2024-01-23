b4f4696f34911efd397296b543f797320dac9790
Frank Ikwuegbu <frankikwuegbu1@gmail.com>
2024-01-22 22:10:24 -0800

    mapping an array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import './index.css';
 import Employee from './components/Employee'
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const[role, setRole] = useState('N/A');
+  const[employees, setEmployees] = useState(
+    [
+      {
+        name: 'Frank Ikwuegbu', 
+        role: 'Senior Dev.', 
+        img:'https://images.pexels.com/photos/936117/pexels-photo-936117.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      },
+      {
+        name: 'Abde Maram',
+        role: 'Senior Dev.',
+        img:'https://images.pexels.com/photos/764529/pexels-photo-764529.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      },
+      {
+        name: 'Abubakar Nazir',
+        role: 'Junior Dev.',
+        img:'https://images.pexels.com/photos/2232981/pexels-photo-2232981.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      },
+      {
+        name: 'Carrie Mathison', 
+        role: 'Junior Dev.', 
+        img:'https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      },
+      {
+        name: 'Afolabi Kehinde-Joyce', 
+        role: 'Senior Dev.', 
+        img:'https://images.pexels.com/photos/773371/pexels-photo-773371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      },
+      {
+        name: 'Aakash Vikram', 
+        role: 'Developer', 
+        img:'https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      },
+  ]
+  );
+
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showEmployees ?
         <>
         <input 
@@ -16,9 +52,21 @@ function App() {
             setRole(e.target.value)
           }}
         />
-          <Employee name = 'Frank' role='Intern' />
-          <Employee name= 'Jenny' role={role}/>
-          <Employee name= 'Vikram' />
+          <div className='flex flex-wrap justify-center'>
+            {employees.map((employee) => {
+              console.log(employee);
+              return(
+                <Employee 
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            }
+            )
+          }
+          </div>
         </>
       :
         <p>access denied</p>
